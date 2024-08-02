e300e0ad00f604aaea70aae7737c055985d2e3d8
Sergio Tomás <sergiota.cs@gmail.com>
2024-08-02 11:01:24 +0200

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
     <div>
-      <h1 className='text-3xl font-bold underline'>
-        Hello World!
-      </h1>
+      <Navbar />
     </div>
   )
 }
